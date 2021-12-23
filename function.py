6b7e1973aea7195c059bf52dbd9903e0b521f21d
Brandon <brandonnicolas827@yahoo.com>
2021-12-23 13:51:44 -0500

    Modify lambda function

--- function.py
+++ function.py
@@ -3,34 +3,18 @@ from __future__ import print_function
 import boto3
 import json
 
-print('Loading function')
-
+table = boto3.resource('dynamodb').Table('resumecounter')
 
 def handler(event, context):
-    '''Provide an event that contains the following keys:
-
-      - operation: one of the operations in the operations dict below
-      - tableName: required for operations that interact with DynamoDB
-      - payload: a parameter to pass to the operation being performed
-    '''
-    #print("Received event: " + json.dumps(event, indent=2))
-
-    operation = event['operation']
-
-    if 'tableName' in event:
-        dynamo = boto3.resource('dynamodb').Table(event['tableName'])
-
-    operations = {
-        'create': lambda x: dynamo.put_item(**x),
-        'read': lambda x: dynamo.get_item(**x),
-        'update': lambda x: dynamo.update_item(**x),
-        'delete': lambda x: dynamo.delete_item(**x),
-        'list': lambda x: dynamo.scan(**x),
-        'echo': lambda x: x,
-        'ping': lambda x: 'pong'
-    }
-
-    if operation in operations:
-        return operations[operation](event.get('payload'))
-    else:
-        raise ValueError('Unrecognized operation "{}"'.format(operation))
+    #when get request is received (which is done everytime this function runs, it runs on get events) by website
+    #update 'visits' variable in dynamodb table
+    VisitCount = table.get_item(Key={'Site'}, ProjectionExpression = 'Visits')
+    
+    table.updateitem(
+        Key = {'Site'},
+        UpdateExpression = "set Visits=:r",
+        ExpressionAttributeValues={
+            ':r': VisitCount + 1
+        },
+        ReturnValues="UPDATED_NEW"
+    )
