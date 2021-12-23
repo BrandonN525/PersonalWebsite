8ae548d3899bc039e37f6b8d8cae60a0beef7151
Brandon <brandonnicolas827@yahoo.com>
2021-12-23 15:25:14 -0500

    Update lambda function

--- function.py
+++ function.py
@@ -8,13 +8,15 @@ table = boto3.resource('dynamodb').Table('resumecounter')
 def handler(event, context):
     #when get request is received (which is done everytime this function runs, it runs on get events) by website
     #update 'visits' variable in dynamodb table
-    VisitCount = table.get_item(Key={'Site'}, ProjectionExpression = 'Visits')
+    #VisitCount = table.get_item(Key = {'Site'}, ProjectionExpression = 'Visits')
     
     table.updateitem(
         Key = {'Site'},
-        UpdateExpression = "set Visits=:r",
-        ExpressionAttributeValues={
-            ':r': VisitCount + 1
-        },
-        ReturnValues="UPDATED_NEW"
+        UpdateExpression = "SET Visits = Visits + 1",
+        #UpdateExpression = "ADD Visits: 1"
+        #UpdateExpression = "Set Visits = :r"
+        #ExpressionAttributeValues={
+        #    ':r': VisitCount.value + 1
+        #},
+        ReturnValues = "UPDATED_NEW"
     )
