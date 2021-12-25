24c933d3284239a89b92b43848ef747af12cfaeb
Brandon <brandonnicolas827@yahoo.com>
2021-12-25 15:54:30 -0500

    Update Lambda function

--- function.py
+++ function.py
@@ -11,11 +11,13 @@ table = dynamodb.Table('resumecounter')
 def handler(event, context):
     #when get request is received update 'visits' variable in dynamodb table
     
-    table.update_item(
+    response = table.update_item(
         Key = {'Site': 0},
         UpdateExpression = 'SET Visits = Visits + :val',
         ExpressionAttributeValues={
             ":val": 1
         },
-        ReturnValues = "NONE"
+        ReturnValues = "UPDATED_NEW"
     )
+
+    return response
