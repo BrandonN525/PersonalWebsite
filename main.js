39f19395977d6094e2d994f457b5e26fbc7583fc
Brandon <brandonnicolas827@yahoo.com>
2022-01-02 17:50:39 -0500

    Resolve 'undefined' error returning from API call to DynamoDB

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ async function getCounter() {
       method: 'GET'
     });
     let data = await response.json();
-    document.getElementById("visits").innerHTML = "Visits: " + data['Visits'];
+    document.getElementById("visits").innerHTML = "Visits: " + data['counter'];
     console.log(data);
     return data;
 }
