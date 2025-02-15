94767f16cc170129bd155f99457c04f70973a574
Brandon <brandonnicolas827@yahoo.com>
2025-02-15 18:57:19 -0500

    Updating comment for clarity

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function setupNavigation() {
   });
 }
 
-// Fetch and animate the visit counter with error handling
+// Fetch the visit counter with error handling
 async function updateCounter() {
   try {
     const response = await fetch('https://2g03j5b0b1.execute-api.us-east-1.amazonaws.com/Prod/counter', {
